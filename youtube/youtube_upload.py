a3bf6a1b481d2fe6ec0616dfbb00d58361cf0b5f
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-06-02 00:02:56 +0530

    Implement YouTube service retrieval function
    Add function to get YouTube service with OAuth2 credentials

--- youtube/youtube_upload.py
+++ youtube/youtube_upload.py
@@ -1,1 +1,27 @@
-# upload funtion for youtube
+
+import os
+
+from google.oauth2.credentials import Credentials
+
+from googleapiclient.discovery import build
+
+from googleapiclient.http import MediaFileUpload
+
+
+TOKEN_FILE = "auth/token.json"
+
+
+def get_youtube_service():
+
+    credentials = Credentials.from_authorized_user_file(
+        TOKEN_FILE,
+        [
+            "https://www.googleapis.com/auth/youtube.upload"
+        ]
+    )
+
+    return build(
+        "youtube",
+        "v3",
+        credentials=credentials
+    )
