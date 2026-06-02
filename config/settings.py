1e94b64e35296dd735354c2cdcddbdaa386f4136
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-06-02 11:42:34 +0530

    Update MASTER_JSON_PATH to relative path

--- config/settings.py
+++ config/settings.py
@@ -3,4 +3,4 @@ import os
 SOURCE_FOLDER_ID = os.getenv("SOURCE_FOLDER_ID")
 PROCESSED_FOLDER_ID = os.getenv("PROCESSED_FOLDER_ID")
 
-MASTER_JSON_PATH = "data/master_videos.json"
+MASTER_JSON_PATH = "../data/master_videos.json"
